8cbafe2a2a2b73506901bf1afb86b70e1745cf86
Ritwik Bisht <ritwik.bisht@gmail.com>
2020-11-20 17:34:16 +0530

    first commit

---
+++ Project/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path
+from WordPredict.views import index,predict
+
+from django.conf.urls import url
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^$',index,name="index"),
+    url(r'^posts/(?P<slug>.+?)/$',predict,name="predict")
+
+]
+
+
